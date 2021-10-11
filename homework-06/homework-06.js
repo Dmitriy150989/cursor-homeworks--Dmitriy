7745fc1b8723c4297d788b3f95349e264a31902b
Dmitriy150989 <Dmitriy150989@gmail.com>
2021-10-11 23:57:51 +0300

    HW-06

--- homework-06/homework-06.js
+++ homework-06/homework-06.js
@@ -1,47 +1,70 @@
-
-// У цьому домашньому завданні вам необхідно зробити мінімум 6 функцій (на вибір)
-
 // Створіть функцію getRandomArray(length, min, max) – яка повертає масив випадкових цілих чисел. У функції є параметри: length - довжина масиву, min – мінімальне значення цілого числа, max – максимальне значення цілого числа. Приклад: getRandomArray(15, 1, 100) –> [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2]
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-	  
-const getRandomArray = (length, min, max) => {
-	let arr = []
-	for (let i = 0; i < length; i++) {
-		arr.push(getRandom(min, max))
-	}
-	return arr
-}
 
+const getRandomArray = (length, min, max) => {
+    let arr = []
+    for (let i = 0; i < length; i++) {
+        arr.push(getRandom(min, max))
+    }
+    return arr
+}
 console.log(getRandomArray(15, 1, 100));
 
-
-
-
-// Створіть функцію getModa(...numbers) – яка вираховує моду всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
-
-// Приклад: getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 2
 // Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
 
-// Приклад: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4
+const numbersInt = (numbers) =>  numbers.filter(n => Number.isInteger(n) ?  n : 0)
+
+const getAverage = (...numbers) => {
+    numbers = numbersInt(numbers);
+    return numbers.reduce((a, b) => a + b ) / numbers.length
+}
+console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+
 // Створіть функцію getMedian(...numbers) – яка рахує медіану всіх переданих в неї аргументів. НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
-
-// Приклад: getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 23 Приклад: getMedian(1, 2, 3, 4) –> 2.5 Приклад: getMedian(1, 2, 3, 4, 5) –> 3
-// Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
-
-// Приклад: filterEvenNumbers(1, 2, 3, 4, 5, 6) -> [1, 3, 5]
+const getMedian = (...numbers) => {
+    numbers = numbersInt(numbers);
+    numbers = numbers.sort((a, b) => a - b);
+    const i = numbers.length / 2;
+    return i % 1 == 0 ? (numbers[i - 1] + numbers[i]) / 2 : numbers[Math.floor(i)];
+}
+console.log(getMedian(1, 2, 3, 4));
 
 // Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
-// Приклад: countPositiveNumbers(1, -2, 3, -4, -5, 6) -> 3
+const countPositiveNumbers = (...numbers) =>  numbers.reduce((a, b) => a + (b > 0 ? 1 : 0));
+console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6));
 
-// Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
 
-// Приклад: getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) -> [55, 55]
-// Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*). При вирішенні цього завдання необхідно розбити масив на слова за допомогою функції .split(" "), після чого масив необхідно буде склеїти .join(" ") Погані слова: shit та fuck. Передбачте можливість розширювати список цих слів у майбутньому.
-// Приклад: replaceBadWords("Are you fucking kidding?") -> "Are you ****ing kidding?" Приклад: replaceBadWords("Holy shit!") -> "Holy ****!" Приклад: replaceBadWords("It's bullshit!") -> "It's bull****!"
 
-// Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох – не розбиває. Пробіли завжди видаляються. Рядок приводится до нижнього регістру. Приклад: divideByThree("Commander) -> ["com", "man", "der"] Приклад: divideByThree("live") -> ["liv", "e"]
 
-// Створіть функцію generateCombinations(word), яка видасть всі можливі перестановки(унікальні, без повторень) букв в слові. Для тестів не передавайте слова в яких більше 10 букв. Краще взагалі обмежити работу функції 10 буквами. Приклад: generateCombinations("man") -> ["man", "mna", "amn", "anm", "nam", "nma"] Приклад: generateCombinations("ol") -> ["ol", "lo"]
+// Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви. Якщо букв менше трьох – не розбиває. Пробіли завжди видаляються. Рядок приводится до нижнього регістру.
+const divideByThree = (word) => word.replace(' ', '').toLowerCase().match(/.{1,3}/g);
+console.log(divideByThree('Comma nder'));
+
+// 8 - Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*).
+function replaceBadWords(string) {
+  const shitWords = ['shit', 'fuck', 'asshole'];
+  let result = string;
+
+  for (let word of shitWords) {
+    let length = word.length;
+    let star = '*'.repeat(length);
+    if (string.toLowerCase().includes(word)) {
+      result = result.toLowerCase().replaceAll(word.toLocaleLowerCase(), star);
+      
+    }
+  };
+  result = result[0].toUpperCase() + result.slice(1);
+  for (let i = 1; i < result.length - 1; i++) {
+    if (result[i] === '?' || result[i] === '!' || result[i] === '.' ) {
+      result = result.replace(result[(i + 2)], result[(i + 2)].toUpperCase());
+    }
+  }
+  return result;
+}
+const onlyGoodWords = replaceBadWords("Are you fucking kidding, shit, fuck? He's an asshole, but you'll get used to it. Shit, shit, fuck!");
+console.log(onlyGoodWords);
+
+
 
 
